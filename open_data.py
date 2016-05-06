abefd573553d45bb819071e36a107ab91352e1e9
Dani <danalitovsky@gmail.com>
2016-05-06 07:26:40 -0500

    merge

--- open_data.py
+++ open_data.py
@@ -16,6 +16,12 @@ for f in files:
     hot = get_artist_hotttnesss(h5)
     hottness_counter += hot
     no_files += 1
+    t = get_artist_terms(h5)
+    no_t = get_artist_terms_freq(h5)
+    w = get_artist_terms_weight(h5)
+    print 'ARTIS TERMS AND FREQ', (t, no_t, w)
+    tags = get_artist_mbtags(h5)
+    print 'ARTIST TAGS', tags
     h5.close()
 
 avg_hottness = round(hottness_counter / no_files, 5)
