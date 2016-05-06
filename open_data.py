9684f07de93f5b2969c0f5a8c848ae7bcb62c538
Dani <danalitovsky@gmail.com>
2016-05-06 11:07:58 -0500

    trying custom protocol

--- open_data.py
+++ open_data.py
@@ -1,15 +1,12 @@
 import glob
 from hdf5 import *
+from mrjob.job import MRJob
 
 
-'''
-File to open sample songs data and access attributes through hdf5 getters
-
-'''
 
 def get_year_terms(song_file):
 
-    h5 = open_h5_file_read(f)
+    h5 = open_h5_file_read(song_file)
     year = get_year(h5)
     terms = get_artist_terms(h5)
     #no_t = get_artist_terms_freq(h5)
@@ -18,51 +15,46 @@ def get_year_terms(song_file):
     h5.close()
     return year, terms
 
-def list_all(maindir):
-    """
-    Goes through all subdirectories, open every song file,
-    and list all artists it finds.
-    It returns a dictionary of string -> tuples:
-       artistID -> (musicbrainz ID, trackID, artist_name)
-    The track ID is random, i.e. the first one we find for that
-    artist. The artist information should be the same in all track
-    files from that artist.
-    We assume one song per file, if not, must be modified to take
-    into account the number of songs in each file.
-    INPUT
-      maindir  - top directory of the dataset, we will parse all
-                 subdirectories for .h5 files
-    RETURN
-      dictionary that maps artist ID to tuple (MBID, track ID, artist name)
-    """
-    results = {}
-    numfiles = 0
-    # iterate over all files in all subdirectories
-    for root, dirs, files in os.walk(maindir):
-        # keep the .h5 files
-        files = glob.glob(os.path.join(root,'*.h5'))
-        for f in files :
-            numfiles +=1
-            # get the info we want
-            aid,ambid,tid,aname = get_artistid_trackid_artistname(f)
-            assert aid != '','null artist id in track file: '+f
-            # check if we know that artist
-            if aid in results.keys():
-                continue
-            # we add to the results dictionary
-            results[aid] = (ambid,tid,aname)
-    # done
-    return results
+
+class H5Protocol(object):
+
+    def read(self, h5):
+
+        # receive opened (orn ot?) h5 file, get attributes and return them
+        year, terms = get_year_terms(h5)
+        print year, terms
+        return year, terms
+
+    def write(self, key, value):
+        return key, value
 
 
-######################
-#change this to the dir where your data is
-files = glob.glob("../samples/*.h5")
+class DecadeTerms(MRJob):
+  '''
 
-for f in files:
-    print get_year_terms(f)
+  '''
+  INPUT_PROTOCOL = H5Protocol
 
-#def get_all_data():
+  
+  def mapper(self, key, val):
+    '''
+    '''
+    print key
+    print val
+    yield key, val
+
+
+if __name__ == '__main__':
+
+    ######################
+    #change this to the dir where your data is
+    #files = glob.glob("../samples/*.h5")
+
+    # for f in files:
+    #     print get_year_terms(f)
+
+    #def get_all_data():
+    DecadeTerms.run()
 
 
 
