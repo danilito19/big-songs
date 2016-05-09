b5a08afe505f60a7b05972fe01e5b8825ad565a5
Dani <danalitovsky@gmail.com>
2016-05-08 23:06:53 -0500

    got word per decade to work

--- dl.py
+++ dl.py
@@ -2,18 +2,6 @@ import glob
 from hdf5 import *
 from mrjob.job import MRJob
 
-decades = {'1920': (1920, 1930),
-        "1930": (1930, 1940),
-        "1940": (1940, 1950),
-        "1950": (1950, 1960),
-        "1960": (1960, 1970),
-        "1970": (1970, 1980),
-        "1980": (1980, 1990),
-        "1990": (1990, 2000),
-        "2000": (2000, 2010),
-        "2010": (2010, 2016)
-    
-}
 
 ### decades 0: 2000, decade 1: 2010 - 
 
@@ -22,25 +10,22 @@ class DecadeTerms(MRJob):
 
     def mapper(self, __, line):
 
-        artist_name, artist_year, artist_terms, hot = line.split("|")
+        artist_name, song_year, artist_terms, hot = line.split("|")
 
-        for t in artist_terms.split(","):
-            yield t.lower(), artist_year
+        # this only yields words with years, does not yields the terms
+        # of songs without years
+        if int(song_year) in range(1920, 2020):
+            decade = int(song_year[2:4])/10
+            for t in artist_terms.split(","):
+                yield decade, t.lower()
 
-    def combiner(self, term, year):
-        for yr in year:
-            if int(yr) in range(1920, 2020):
-                decade = int(yr[2:4])/10
-                yield term, decade
+    def combiner(self, decade, terms):
+        for t in terms:
+            yield decade, t
 
+    def reducer(self, decade, terms):
+        yield decade, list(terms)
 
-    def reducer(self, term, decade):
-        # print term, list(decade)
-        for d in decade:
-            yield d, term
-
-
-    # create a step and another reducer to give list of words per decade
 
 if __name__ == '__main__':
 
